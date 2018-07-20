05e62517fef5c781bc7cf703de7f398f20623df5
Chris Powell <chris.ian.powell@gmail.com>
2018-07-20 10:48:30 -0600

    adds first user story

--- spec/features/vending_machine_page_spec.rb
+++ spec/features/vending_machine_page_spec.rb
@@ -1,7 +1,17 @@
 describe 'Vending machine page' do
   context 'A user' do
     it 'visits a specific vending machine page and sees all snack for that machine' do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      snack_one = dons.snacks.create(name: "Cherry", price: 1)
+      snack_two = dons.snacks.create(name: "Choco", price: 2)
 
+      visit machine_path(dons)
+
+      expect(page).to have_content("Snack: #{snack_one.name}")
+      expect(page).to have_content("Price: #{snack_one.price}")
+      expect(page).to have_content("Snack: #{snack_one.name}")
+      expect(page).to have_content("Price: #{snack_one.price}")
     end
   end
 end
